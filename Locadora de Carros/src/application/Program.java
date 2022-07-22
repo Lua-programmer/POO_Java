d3e5fa889f10ee928bdab3e419fece482a6fabc2
Luana Melissa <luanamelissaprogrammer@gmail.com>
2022-07-22 16:58:51 -0300

    Resolução do exercicio 1 - sem interfaces

--- Locadora de Carros/src/application/Program.java
+++ Locadora de Carros/src/application/Program.java
@@ -1,7 +1,49 @@
 package application;
 
-public class Program {
-    public static void main(String[] args) {
+import entities.AluguelDoCarro;
+import entities.Veiculo;
+import services.ServicoAluguel;
+import services.TaxaServico;
 
+import java.io.PrintStream;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) throws ParseException {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyy HH:ss");
+        PrintStream show = System.out;
+
+        show.println("Entre com os dados do aluguel:");
+        show.print("Modelo do carro: ");
+        String modelo = sc.nextLine();
+        show.print("Check-in: (dd/MM/yyy HH:ss) ");
+        Date inicio = sdf.parse(sc.nextLine());
+        show.print("Check-out: (dd/MM/yyy HH:ss) ");
+        Date fim = sdf.parse(sc.nextLine());
+
+        AluguelDoCarro aluguelDoCarro = new AluguelDoCarro(inicio, fim, new Veiculo(modelo));
+
+        show.println();
+        show.print("Entre com o valor do preço por hora: R$ ");
+        double precoHora = sc.nextDouble();
+        show.print("Entre com o valor do preço por dia: R$ ");
+        double precoDia = sc.nextDouble();
+
+        ServicoAluguel servicoAluguel = new ServicoAluguel(precoDia, precoHora, new TaxaServico());
+
+        servicoAluguel.processamentoFatura(aluguelDoCarro);
+
+        show.println("FATURA:");
+        show.println("Pagamento básico: " + String.format("%.2f", aluguelDoCarro.getFatura().getPagamentoBasico()));
+        show.println("Taxa: " + String.format("%.2f", aluguelDoCarro.getFatura().getTaxa()));
+        show.println("Pagamento final: " + String.format("%.2f", aluguelDoCarro.getFatura().getPagamentoTotal()));
+
+        sc.close();
     }
 }
--- Locadora de Carros/src/services/ServicoAluguel.java
+++ Locadora de Carros/src/services/ServicoAluguel.java
@@ -1,6 +1,7 @@
 package services;
 
 import entities.AluguelDoCarro;
+import entities.Fatura;
 
 public class ServicoAluguel {
     private Double precoDia;
@@ -41,6 +42,20 @@ public class ServicoAluguel {
     //metodo responsavel por gerar a nota de pagamento
 
     public void processamentoFatura(AluguelDoCarro aluguelDoCarro) {
+        long t1 = aluguelDoCarro.getInicio().getTime(); //pega o valor em milisegundos da data
+        long t2 = aluguelDoCarro.getFim().getTime();
+        //t2-t1 vai achar a diferenca em milisegundos / 1000 para segundos /60 para minutos /60 para horas
+        double horas = (double) (t2 - t1) / 1000 / 60 / 60;
 
+        double pagamentoBasico;
+        if (horas <= 12) {
+            pagamentoBasico = Math.ceil(horas) * precoHora; //Math.ceil vai arredondar a hora pra cima
+        } else {
+            pagamentoBasico = Math.ceil(horas / 24) * precoDia; //Math.ceil vai arredondar o dia pra cima
+        }
+
+        double taxa = taxaServico.taxa(pagamentoBasico);
+
+        aluguelDoCarro.setFatura(new Fatura(pagamentoBasico, taxa));
     }
 }
